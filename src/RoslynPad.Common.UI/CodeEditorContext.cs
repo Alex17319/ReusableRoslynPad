ef69fafe545c2890be3db557ba85557846e5467d
Alex Priest <prie0075@asms.sa.edu.au>
2017-07-16 00:26:59 +0930

    Added and started to implement CodeEditorContext. DocumentRoot design seems to be problematic. Possibly null should not be allowed in OpenDocumentViewModel.SetDocument(), and DocumentViewModel.CreateNew() should be used by MainViewModel.CreateNewDocument() (idk if this would save the document earlier than is wanted though). This would hopefully avoid OpenDocumentViewModel needing access to DocumentRoot, or ideally anything other than the current document

---
+++ src/RoslynPad.Common.UI/CodeEditorContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoslynPad.Roslyn;
+
+namespace RoslynPad.UI
+{
+	public class CodeEditorContext
+	{
+		public RoslynHost RoslynHost { get; }
+
+		public NuGetConfiguration NuGetConfiguration { get; }
+
+		public double MinEditorFontSize { get; }
+        public double MaxEditorFontSize { get; }
+
+		private double _editorfontSize;
+		public double EditorFontSize {
+			get => _editorfontSize;
+			set {
+				var newVal = Math.Max(Math.Min(value, MaxEditorFontSize), MinEditorFontSize);
+
+				if (value != _editorfontSize) {
+					_editorfontSize = value;
+					EditorFontSizeChanged?.Invoke(value);
+				}
+			}
+		}
+		public event Action<double> EditorFontSizeChanged;
+
+		public CodeEditorContext(RoslynHost roslynHost, NuGetConfiguration nuGetConfiguration, double minEditorFontSize, double maxEditorFontSize, double editorFontSize)
+		{
+			this.RoslynHost = roslynHost ?? throw new ArgumentNullException(nameof(roslynHost));
+			this.NuGetConfiguration = nuGetConfiguration ?? throw new ArgumentNullException(nameof(nuGetConfiguration));
+
+			this.MinEditorFontSize = minEditorFontSize;
+			this.MaxEditorFontSize = maxEditorFontSize;
+			this.EditorFontSize = editorFontSize; //Clips values automatically, event is null at this point
+
+
+		}
+	}
+}
